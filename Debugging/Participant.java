c3b9d0da6fd4455c95ed1f81b95aaac4e2c0d377
danny <danny.connolly@my.jcu.edu.au>
2017-10-07 14:20:04 +1000

    add Participant
    used the solutions to much could not work it out

---
+++ Debugging/Participant.java
@@ -0,0 +1,40 @@
+/**
+ * Created by danny on 7/10/2017.
+ */
+public class Participant
+{
+    private String name;
+    private int age;
+    private String address;
+    public Participant(String n, int a, String add)
+    {
+        name = name;
+        age = age;
+        address = address;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getAge()
+    {
+        return age;
+    }
+    public String getAddress()
+    {
+        return address;
+    }
+    public String toString()
+    {
+        return name + " " + age + " years old " + address;
+    }
+    public boolean equals(Participant p)
+    {
+        boolean areEqual = false;
+        if(name.equals(p.getName()))
+            if(age == p.getAge())
+                if(address.equals((p.getAddress())))
+                    areEqual = true;
+        return areEqual;
+    }
+}
